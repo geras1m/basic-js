2ff79c5912eccda9a1016bf60fb456166165a946
Anton Haldouski <anton94-94@list.ru>
2022-10-15 20:01:21 +0300

    feat: solution of "Dream team"

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false
+    }
+
+    return members.reduce((acc, item) => {
+        if (typeof item === 'string') {
+            item = item.trim().toUpperCase()[0];
+            acc.push(item[0])
+        }
+        return acc.sort()
+    }, []).join('')
 }
 
+// console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']))
+console.log(createDreamTeam([
+    '   William Alston ',
+    ' Paul Benacerraf',
+    '  Ross Cameron',
+    '       Gilles Deleuze',
+    '  Arda Denkel ',
+    '  Michael Devitt',
+    '  Kit Fine',
+    ' Nelson Goodman',
+    'David Kolb',
+    '   Saul Kripke',
+    '  Trenton Merricks',
+    '  Jay Rosenberg',
+]))
+
+
 module.exports = {
-  createDreamTeam
+    createDreamTeam
 };
