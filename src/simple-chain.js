b154241e12064ebbfc359876976806c4a2e32fbd
Anton Haldouski <anton94-94@list.ru>
2022-10-16 16:16:30 +0300

    feat: solution of " Make chain!"

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arrayOfChains: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arrayOfChains.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink( value ) {
+    this.arrayOfChains.push(`( ${String(value)} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink( position ) {
+    // if(typeof position !== 'number' || position % 1 !== 0 || position - 1 < 0 || position - 1 > this.arrayOfChains.length){
+    //   throw new Error("You can't remove incorrect link!");
+    // }
+    if(typeof position === 'number' && position % 1 === 0 && position - 1 >= 0 && position - 1 < this.arrayOfChains.length) {
+      this.arrayOfChains.splice(position - 1, 1);
+      return this;
+    }
+    this.arrayOfChains = []
+    throw new Error("You can't remove incorrect link!");
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arrayOfChains.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    console.log(this.arrayOfChains)
+    let str = this.arrayOfChains.join('~~')
+    this.arrayOfChains = []
+    return str;
   }
 };
+
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).finishChain())
+// console.log(chainMaker.addLink(1).addLink(2).removeLink(1).addLink(3).finishChain())
+console.log(chainMaker.addLink(1).addLink(2).reverseChain().addLink(3).finishChain())
+console.log(chainMaker.addLink(1).addLink(2).addLink(3).finishChain())
+
+/*
+getLength возвращает текущую длину цепочки в виде числа;
+addLink(value)добавляет ссылку, содержащую string представление в value цепочку;
+removeLink(position)убирает звено цепи в указанном положении;
+reverseChain переворачивает цепь;
+finishChain заканчивается цепочка и returns она.
+*/
 
 module.exports = {
   chainMaker
